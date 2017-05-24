a3dfc68d07910f497047b264c27cc68250034d38
Sandra Keßler <sk@softwarehaus-kassel.de>
2017-05-24 11:34:24 +0200

    commented menuAttributes

--- app/src/main/java/org/monkey/d/ruffy/ruffy/driver/display/MenuAttribute.java
+++ app/src/main/java/org/monkey/d/ruffy/ruffy/driver/display/MenuAttribute.java
@@ -5,16 +5,24 @@ package org.monkey.d.ruffy.ruffy.driver.display;
  */
 
 public enum MenuAttribute {
-    RUNTIME,
-    BOLUS,
-    BOLUS_REMAINING,
-    TBR,
-    BASAL_RATE,
-    BASAL_SELECTED,
-    LOW_BATTERY,
-    LOW_INSULIN,
-    LOCK_STATE,
-    MULTIWAVE_BOLUS,
-    BOLUS_TYPE,
-    TIME, REMAINING_INSULIN,
+    RUNTIME,//runtime of current operation, remaining time on main menu
+    BOLUS,//double units
+    BOLUS_REMAINING,//double units remain from current bolus
+    TBR,//double 0-500%
+    BASAL_RATE,//double units/h
+    BASAL_SELECTED,//int selected basal profile
+    LOW_BATTERY,//boolean low battery warning
+    LOW_INSULIN,//boolean low insulin warning
+    LOCK_STATE,//int keylock state 0==no lock, 1==unlocked, 2==locked
+    MULTIWAVE_BOLUS,//double immediate bolus on multiwave
+    BOLUS_TYPE,//BolusType, only history uses MULTIWAVE
+    TIME,//time MenuTime
+    REMAINING_INSULIN,//double units
+    DATE,//date MenuDate
+    CURRENT_RECORD,//int current record
+    TOTAL_RECORD, //int total num record
+    ERROR, //int errorcode
+    WARNING, //int errorcode
+    MESSAGE, //string errormessage
+    DAILY_TOTAL,//double units
 }
