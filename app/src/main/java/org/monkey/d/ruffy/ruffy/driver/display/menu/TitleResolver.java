4b4725068a524e63168d73d71ae089387fb93262
AdrianLxM <adrian.m@inbox.com>
2017-06-01 23:52:59 +0200

    hungarian

--- app/src/main/java/org/monkey/d/ruffy/ruffy/driver/display/menu/TitleResolver.java
+++ app/src/main/java/org/monkey/d/ruffy/ruffy/driver/display/menu/TitleResolver.java
@@ -298,6 +298,28 @@ class TitleResolver {
         if(title.equalsIgnoreCase("mbr-varighed")) //TBR 2
             return Title.TBR_DURATION;
 
+        /**hungarian titles**/
+        if(title.equalsIgnoreCase("bólusmennyiség")) //multiwave 1
+            return Title.BOLUS_AMOUNT;
+        if(title.equalsIgnoreCase("azonnali bólus")) //multiwave 2
+            return Title.IMMEDIATE_BOLUS;
+        if(title.equalsIgnoreCase("bólus időtartam")) //multiwave 3
+            return Title.BOLUS_DURATION;
+        if(title.equalsIgnoreCase("quick info")) //check1
+            return Title.QUICK_INFO;
+        if(title.equalsIgnoreCase("bólusadatok")) //check2, mydata 1
+            return Title.BOLUS_DATA;
+        if(title.equalsIgnoreCase("hibaadatok")) //mydata 2
+            return Title.ERROR_DATA;
+        if(title.equalsIgnoreCase("napi teljes")) //mydata 3
+            return Title.DAILY_TOTALS;
+        if(title.equalsIgnoreCase("tbr-adatok")) //mydata 4
+            return Title.TBR_DATA;
+        if(title.equalsIgnoreCase("tbr százalék")) //TBR 1
+            return Title.TBR_SET;
+        if(title.equalsIgnoreCase("tbr időtartam")) //TBR 2
+            return Title.TBR_DURATION;
+
         //FIXME add Translations
         return null;
     }
