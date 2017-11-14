419353347580c67268294f388601e824de930041
Johannes Mockenhaupt <git@jotomo.de>
2017-11-14 10:53:57 +0100

    Selectively take over upstream changes till bdbeaa3.

--- app/src/main/java/org/monkey/d/ruffy/ruffy/driver/Ruffy.java
+++ app/src/main/java/org/monkey/d/ruffy/ruffy/driver/Ruffy.java
@@ -60,6 +60,10 @@ public class Ruffy extends Service  {
         @Override
         public int doRTConnect() throws RemoteException {
             step= 0;
+            if(isConnected()) {
+                rtHandler.rtStarted();
+                return 0;
+            }
             if(Ruffy.this.rtHandler==null)
             {
                 return -2;//FIXME make errors
@@ -110,14 +114,19 @@ public class Ruffy extends Service  {
                     // it's not going better here either
                 }
             }
-*/
 
             synRun=false;
             rtModeRunning =false;
         }
 
-        public boolean isConnected() {
-            return btConn != null && btConn.isConnected();
+        public boolean isConnected()
+        {
+            if (btConn!=null) {
+                return btConn.isConnected();
+            } else {
+                return false;
+            }
+
         }
     };
 
